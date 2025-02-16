c2ae2f38487ba5846fd6d712383f0f37ae7a5e8e
Donna Szeto <szetodonna2@gmail.com>
2025-02-15 19:44:54 -0800

    fixed collision calculation

--- index.js
+++ index.js
@@ -220,13 +220,13 @@ npcs.forEach(npc => {
 
 const movables = [background, ...boundaries, foreground, extraForegroundObjects, ...npcs]
 
-function rectangularCollision({rectangle1, rectangle2}) {
+function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
-    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
-    rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
-    rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
-    rectangle1.position.y <= rectangle2.position.y + rectangle2.height
-  )
+    rectangle1.position.x < rectangle2.position.x + rectangle2.width &&
+    rectangle1.position.x + rectangle1.width > rectangle2.position.x &&
+    rectangle1.position.y < rectangle2.position.y + rectangle2.height &&
+    rectangle1.position.y + rectangle1.height > rectangle2.position.y
+  );
 }
 
 let dialogueIndex = 0;
