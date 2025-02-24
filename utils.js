33860de468523dd8d4b354ddb30218ce3ef30aa6
Donna Szeto <szetodonna2@gmail.com>
2025-02-24 14:35:12 -0800

    moved game files to new folder

--- utils.js
+++
@@ -1,8 +0,0 @@
-export function rectangularCollision({ rectangle1, rectangle2 }) {
-  return (
-    rectangle1.position.x < rectangle2.position.x + rectangle2.width &&
-    rectangle1.position.x + rectangle1.width > rectangle2.position.x &&
-    rectangle1.position.y < rectangle2.position.y + rectangle2.height &&
-    rectangle1.position.y + rectangle1.height > rectangle2.position.y
-  );
-}
